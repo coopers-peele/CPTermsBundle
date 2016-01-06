b9783c8a0c0898b1ce9ceebef4d5fafc7349c962
Olivier Pichon <op@united-asian.com>
2016-01-06 14:23:39 +0700

    prevented finalized terms from being modified

--- Controller/AdminController.php
+++ Controller/AdminController.php
@@ -152,7 +152,7 @@ class AdminController extends Controller
      */
     public function editAction(Request $request, $id)
     {
-        $terms = $this->getTerms($id, false);
+        $terms = $this->getTerms($id, true);
 
         $form = $this->createForm(
             new TermsFormType(),
